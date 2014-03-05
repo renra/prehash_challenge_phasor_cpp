4fd27f1be5e54f51d7aa6157f67c4479c3a954ac
Renra <jan.renra.gloser@gmail.com>
2014-03-05 20:32:29 +0100

    Added comments

--- test_opts.cpp
+++ test_opts.cpp
@@ -1,12 +1,14 @@
-#include "./testing_methods.cpp"
+#include "./testing_methods.cpp"  //See the tests in this file
 
+// Uncomment the following tests one by one
+// they will guide you through the implementation
 void run_tests(SimpleTest * test_wrapper){
   test_rect_init_real(test_wrapper);
-  test_rect_init_imag(test_wrapper);
-  test_pol_init_mag(test_wrapper);
-  test_pol_init_phase(test_wrapper);
-  test_rect2pol_mag(test_wrapper);;
-  test_rect2pol_phase(test_wrapper);
-  test_pol2rect_real(test_wrapper);
-  test_pol2rect_imag(test_wrapper);
+  //test_rect_init_imag(test_wrapper);
+  //test_pol_init_mag(test_wrapper);
+  //test_pol_init_phase(test_wrapper);
+  //test_rect2pol_mag(test_wrapper);;
+  //test_rect2pol_phase(test_wrapper);
+  //test_pol2rect_real(test_wrapper);
+  //test_pol2rect_imag(test_wrapper);
 }
