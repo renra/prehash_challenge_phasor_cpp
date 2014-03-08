7c347832a524da2ad59d07e78916ab24203cf61f
Renra <jan.renra.gloser@gmail.com>
2014-03-08 17:43:10 +0100

    Phasor stub

--- phasor.cpp
+++ phasor.cpp
@@ -1,38 +1,19 @@
 #include <cmath>
 
 class Phasor{
-  double real;
-  double imag;
-  double magnitude;
-  double phase;
-
-  void compute_magnitude_and_phase(){
-  }
-
-  void compute_real_and_imag(){
-  }
-
-  double phase_in_radians() const{
-    return 0;
-  }
-
   public:
-  Phasor(double param1, double param2, bool polar = false){
-  }
 
-  double get_real() const {
-    return -1;
-  }
+  //These methods are stubs to prevent compilation errors
+  // make sure you tweak their prototypes too, where appropriate
+  // make sure to add new methods that you think should be here
+  Phasor(double param1, double param2, bool rectangular = false) {}
+  double get_magnitude() {return -1;}
+  double get_phase() {return -1;}
+  double get_real() {return -1;}
+  double get_imag() {return -1;}
 
-  double get_imag() const {
-    return -1;
-  }
-
-  double get_magnitude() const{
-    return -1;
-  }
-
-  double get_phase() const{
-    return -1;
-  }
+  Phasor operator+(Phasor operand) { return Phasor(1,1); }
+  Phasor operator-(Phasor operand) { return Phasor(1,1); }
+  Phasor operator*(Phasor operand) { return Phasor(1,1); }
+  Phasor operator/(Phasor& operand) { return Phasor(1,1); }
 };
